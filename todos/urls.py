4b660dc1a85c6b2ca041efdec906e2a649e31075
qlifford <otienocee@gmail.comgit config --global user.nameqlofford>
2023-09-03 11:50:27 +0300

    2nd commit

--- todos/urls.py
+++ todos/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import TaskList, TaskDetail
+from .views import TaskList, TaskDetail, TaskCreate, UpdateView
 
 urlpatterns = [
-    path('', TaskList.as_view(), name="tasks"),
-    path('todos/<int:pk>/', TaskDetail.as_view(), name="task"),
+    path('', TaskList.as_view(), name="todos"),
+    path('create/', TaskCreate.as_view(), name="todos-create"),
+    path('todos/<int:pk>/', TaskDetail.as_view(), name="todos-get"),
+    path('todos/<int:pk>/', UpdateView.as_view(), name="todos-update"),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
 from .models import Task
+
+from django.urls import reverse_lazy
 
 # Create your views here.
 class TaskList(ListView):
@@ -11,5 +14,18 @@ class TaskList(ListView):
 
 class TaskDetail(DetailView):
     model = Task
-    context_object_name = 'task'
+    context_object_name = 'todos-get'
     template_name = 'todos/details.html'
+
+class TaskCreate(CreateView):
+    model = Task
+    context_object_name = 'todos-create'
+    template_name = 'todos/create.html'
+    fields = '__all__'
+    success_url = reverse_lazy('todos')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('todos')
+
