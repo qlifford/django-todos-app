88c8a9edf3f15d701aeeca34edad808744a60ff4
qlifford <otienocee@gmail.comgit config --global user.nameqlofford>
2023-09-03 21:45:21 +0300

    crud for todo

--- todos/urls.py
+++ todos/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, UpdateView
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
 
 urlpatterns = [
-    path('', TaskList.as_view(), name="todos"),
-    path('create/', TaskCreate.as_view(), name="todos-create"),
-    path('todos/<int:pk>/', TaskDetail.as_view(), name="todos-get"),
-    path('todos/<int:pk>/', UpdateView.as_view(), name="todos-update"),
+    path('', TaskList.as_view(), name="tasks"),
+    path('task-create/', TaskCreate.as_view(), name="task-create"),
+    path('task/<int:pk>/', TaskDetail.as_view(), name="task"),
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name="task-update"),
+    path('task-delete/<int:pk>/', TaskDelete.as_view(), name="task-delete"),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Task
 
 from django.urls import reverse_lazy
@@ -9,23 +9,26 @@ from django.urls import reverse_lazy
 # Create your views here.
 class TaskList(ListView):
     model = Task
-    context_object_name = 'todos'
+    context_object_name = 'tasks'
 
 
 class TaskDetail(DetailView):
     model = Task
-    context_object_name = 'todos-get'
-    template_name = 'todos/details.html'
+    context_object_name = 'task'
+    template_name = 'todos/task_detail.html'
 
 class TaskCreate(CreateView):
     model = Task
-    context_object_name = 'todos-create'
-    template_name = 'todos/create.html'
     fields = '__all__'
-    success_url = reverse_lazy('todos')
+    success_url = reverse_lazy('tasks')
 
 class TaskUpdate(UpdateView):
     model = Task
     fields = '__all__'
-    success_url = reverse_lazy('todos')
+    success_url = reverse_lazy('tasks')
 
+class TaskDelete(DeleteView):
+    model = Task
+    fields = '__all__'
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
